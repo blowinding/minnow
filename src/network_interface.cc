28a4b8180cd62c5879e6a9b0df9f7b7daf03737b
blowinding <2475258425@qq.com>
2024-07-30 18:40:26 +0800

    check5

--- src/network_interface.cc
+++ src/network_interface.cc
@@ -27,21 +27,75 @@ NetworkInterface::NetworkInterface( string_view name,
 //! can be converted to a uint32_t (raw 32-bit IP address) by using the Address::ipv4_numeric() method.
 void NetworkInterface::send_datagram( const InternetDatagram& dgram, const Address& next_hop )
 {
-  // Your code here.
-  (void)dgram;
-  (void)next_hop;
+  // get next_hop ip address
+  uint32_t ip_numeric = next_hop.ipv4_numeric();
+  auto it_res = map_ip_.find(ip_numeric);
+  EthernetFrame frame {};
+  // if not learning arp mapping or arp mapping timeout
+  if (it_res == map_ip_.end() || cur_time_ - it_res->second.learning_time_ >= ARP_TIMEOUT_) {
+    // cannot find phy address, queue frame, and send arp request
+    map_queue_[ip_numeric].push(dgram);
+    // check if arp request too frequently
+    auto it_send_res = map_send_time_.find(ip_numeric);
+    if (it_send_res != map_send_time_.end()) {
+      if (cur_time_ - it_send_res->second <= ARP_INTERVAL_)
+        return;
+    } else {
+      map_send_time_[ip_numeric] = cur_time_;
+    }
+    auto ARP_msg = genArpEthernetFrame(ARPMessage::OPCODE_REQUEST, ethernet_address_,
+                                        { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, ip_address_.ipv4_numeric(), ip_numeric);
+    datagramToEthernetFrame<ARPMessage>(frame, ARP_msg, ETHERNET_BROADCAST, ethernet_address_, EthernetHeader::TYPE_ARP);
+  } else {
+    // ipv4 dgram
+    datagramToEthernetFrame<InternetDatagram>(frame, dgram, it_res->second.mac_addr_, ethernet_address_, EthernetHeader::TYPE_IPv4);
+  }
+  // transmit
+  transmit(frame);
 }
 
 //! \param[in] frame the incoming Ethernet frame
 void NetworkInterface::recv_frame( const EthernetFrame& frame )
 {
-  // Your code here.
-  (void)frame;
+  auto& header = frame.header;
+  // check receive
+  auto dst = header.dst;
+  if (dst != ethernet_address_ && dst != ETHERNET_BROADCAST) {
+    return;
+  }
+  auto& payload = frame.payload;
+  Parser parser(payload);
+  if (header.type == EthernetHeader::TYPE_IPv4) {
+    // ipv4 dgram push into queue
+    InternetDatagram dgram {};
+    dgram.parse(parser);
+    if (!parser.has_error()) {
+      datagrams_received_.push(std::move(dgram));
+    }
+  } else if (header.type == EthernetHeader::TYPE_ARP) {
+    // arp msg process
+    ARPMessage arpMessage {};
+    arpMessage.parse(parser);
+    if (!parser.has_error()) {
+      // if somebody request, send reply
+      if(arpMessage.opcode == ARPMessage::OPCODE_REQUEST && arpMessage.target_ip_address == ip_address_.ipv4_numeric()) {
+        ARPMessage arpReplyMsg = genArpEthernetFrame(ARPMessage::OPCODE_REPLY, ethernet_address_,
+                                                      arpMessage.sender_ethernet_address, ip_address_.ipv4_numeric(), arpMessage.sender_ip_address);
+        EthernetFrame replyFrame {};
+        datagramToEthernetFrame<ARPMessage>(replyFrame, arpReplyMsg, arpMessage.sender_ethernet_address, ethernet_address_, EthernetHeader::TYPE_ARP);
+        transmit(replyFrame);
+      }
+      map_ip_[arpMessage.sender_ip_address] = {
+        .mac_addr_ = arpMessage.sender_ethernet_address,
+        .learning_time_ = cur_time_
+      };
+      transmitDgramInQueue(arpMessage.sender_ip_address, arpMessage.sender_ethernet_address);
+    }
+  }
 }
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
 void NetworkInterface::tick( const size_t ms_since_last_tick )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
+  cur_time_ += ms_since_last_tick;
 }
--- src/network_interface.hh
+++ src/network_interface.hh
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <queue>
+#include <iomanip>
+#include <sstream>
+#include <unordered_map>
+#include <concepts>
 
 #include "address.hh"
 #include "ethernet_frame.hh"
+#include "ethernet_header.hh"
 #include "ipv4_datagram.hh"
 
 // A "network interface" that connects IP (the internet layer, or network layer)
@@ -27,6 +32,10 @@
 // the network interface passes it up the stack. If it's an ARP
 // request or reply, the network interface processes the frame
 // and learns or replies as necessary.
+template<typename T>
+concept isDgram = requires(T t, Serializer& s) {
+  { t.serialize(s) } -> std::same_as<void>;
+};
 class NetworkInterface
 {
 public:
@@ -65,6 +74,12 @@ public:
   OutputPort& output() { return *port_; }
   std::queue<InternetDatagram>& datagrams_received() { return datagrams_received_; }
 
+  // ethernet address and timeout
+  struct MacAddrUnit {
+    EthernetAddress mac_addr_ {};
+    uint64_t learning_time_ {};
+  };
+
 private:
   // Human-readable name of the interface
   std::string name_;
@@ -81,4 +96,59 @@ private:
 
   // Datagrams that have been received
   std::queue<InternetDatagram> datagrams_received_ {};
+
+  constexpr static uint64_t ARP_INTERVAL_ =  5000;
+  constexpr static uint64_t ARP_TIMEOUT_  = 30000;
+
+  // current time
+  uint64_t cur_time_ {};
+
+  // map between mac address and ip address
+  std::unordered_map<uint32_t, MacAddrUnit> map_ip_ {};
+
+  // map between ip address and ipv4 dgram queue
+  std::unordered_map<uint32_t, std::queue<InternetDatagram>> map_queue_ {};
+
+  // map between ip address and arp send time
+  std::unordered_map<uint32_t, uint64_t> map_send_time_ {};
+
+  template<isDgram T>
+  static void datagramToEthernetFrame(EthernetFrame& ethernetFrame, const T& internetDatagram, const EthernetAddress& dst, const EthernetAddress& src, const uint64_t& type) {
+    Serializer serializer;
+    internetDatagram.serialize(serializer);
+    // header
+    auto& header = ethernetFrame.header;
+    header.src = src;
+    header.dst = dst;
+    header.type = type;
+    // frame
+    ethernetFrame.payload = serializer.output();
+  }
+
+  static ARPMessage genArpEthernetFrame(uint16_t opcode, const EthernetAddress& src, const EthernetAddress& dst,
+                                         const uint32_t src_ip, const uint32_t dst_ip,
+                                         uint16_t hardwareType = ARPMessage::TYPE_ETHERNET, uint16_t protocolType = EthernetHeader::TYPE_IPv4) {
+    ARPMessage arpMessage {};
+    arpMessage.hardware_type = hardwareType;
+    arpMessage.protocol_type = protocolType;
+    arpMessage.opcode = opcode;
+    arpMessage.sender_ip_address = src_ip;
+    arpMessage.target_ip_address = dst_ip;
+    arpMessage.sender_ethernet_address = src;
+    arpMessage.target_ethernet_address = dst;
+    return arpMessage;
+  }
+
+  void transmitDgramInQueue(uint32_t sender_ip_address, const EthernetAddress& senderMac) {
+    auto& senderQueue = map_queue_[sender_ip_address];
+    while (!senderQueue.empty()) {
+      auto& dgram = senderQueue.front();
+      EthernetFrame frame {};
+      datagramToEthernetFrame(frame, dgram, senderMac, ethernet_address_, EthernetHeader::TYPE_IPv4);
+      transmit(frame);
+      senderQueue.pop();
+    }
+  }
+
+
 };
